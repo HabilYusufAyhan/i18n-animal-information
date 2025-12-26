6df203af12a840f5522a0d26ac4f9bd97cd3deee
Habil Yusuf Ayhan <habilyusuf20@gmail.com>
2025-12-26 11:00:07 +0300

    first commit

---
+++ src/pages/App.jsx
@@ -0,0 +1,12 @@
+import { use, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [animal, setAnimal] = useState(null);
+
+  return <div></div>;
+}
+
+export default App;
